927cb0fca4960194a2d0b98154c4216beda5a681
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-05 20:18:46 -0300

    clase 15

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// call.withPromise("pedro", "alfonso").then(res => console.log(`Hola ${res}`));
+
+async function x() {
+  let resultado = await call.withPromise("pedro", "rodriguez");
+  console.log(`Hola desde x ${resultado}`);
+}
+
+x();
+
+call.greet("hola");
