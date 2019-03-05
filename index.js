bb20d54026e0590f46174090735a019c09fa166a
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-05 19:53:04 -0300

    clas 14

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// call.withCallback("pedrito", call.sync);
+call.withPromise("pedr qweo").
+  then(a => console.log(a));
