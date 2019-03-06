bd697e37eeeaf80543f689440b86aa9679eaeb45
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-06 12:54:58 -0300

    clase 18

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process');
+
+function execCommand(command) {
+    cp.exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log(`Error: ${err}`)
+        }
+        if (stdout) {
+            console.log(`Standar out: \n ${stdout}`)
+            console.log(`Standar err: \n ${stderr}`)
+
+        }
+    })
+}
+
+// execCommand('ls');
+execCommand('node ./src/script.js --base=5')
