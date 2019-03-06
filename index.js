1fa40b9d7dd7710d51f986beccfc77101344e9ad
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-06 11:10:33 -0300

    Clase 17

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require('fs')
+const ops = require('./src/fileops');
+
+let incValue;
+
+fs.readFile('./resources/number.txt', 'utf8', (err, text) => {
+    if (err) throw err;
+    const number = text.split('\n').map(n => Number(n));
+    console.log('-----------------')
+    console.log(typeof number + ' number');
+    console.log(number);
+    incValue = ops.incrementValues(number);
+    console.log('-----------------')
+
+    fs.writeFile('./resources/numbernew.txt', incValue.join('\n'), (err, result) => {
+        if (err) throw err;
+    
+    })
+    
+});
+
+
+
+
+console.log('aca');
