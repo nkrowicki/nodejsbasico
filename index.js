695a9d20fce853d18f75a4faaab0d72b41f6ca22
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-06 12:34:32 -0300

    clase 17

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.close();
+
+const q = [
+    "Cual es tu primer nombre?",
+    "Cual es tu primer apellido?",
+    "Cual es tu edad? "
+]
+
+const AskQuestion = (rl, question) => {
+    return new Promise((resolve, rej) => {
+        rl.question(question, answer => {
+            resolve(answer)
+        })
+    })
+}
+
+const Ask = (questions) => {
+    return new Promise(async resolve => {
+        let results = [];
+        for (let i = 0; i < questions.length; i++) {
+            const result = await AskQuestion(rl, questions[i])
+            results.push(result)
+        }
+        resolve(results)
+    })
+}
+
+Ask(q)
+    .then(q => {
+        console.log(typeof q)
+        console.log(q)
+        console.log(`Hola ${q[0]} ${q[1]}, tu edad es: ${q[2]}`)
+    })
