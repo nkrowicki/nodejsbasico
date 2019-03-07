722a551b4910a25a4ce24f8d53f1d965de6ca4ce
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-07 16:49:55 -0300

    clase 25

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Estoy en la ruta HOME")
+});
+
+app.get("/home2", (req, res) => {
+    res.send("Estoy en la ruta HOME2")
+});
+
+app.get("*", (req, res) => {
+    res.send("Error ruta *")
+});
+
+app.listen(3000, () => {
+  console.log("Servidor express en escucha en el puerto 3000");
+});
