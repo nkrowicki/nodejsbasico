2c7da50e7ee1b5d2734c30e6a4ae54f5e5a5bf55
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-07 09:09:46 -0300

    clase 21

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require('./src/greet');
+
+greet.emit('call', 'jorge', (name) => {
+    if (1 < 2) console.log(`es ${name}`)
+});
