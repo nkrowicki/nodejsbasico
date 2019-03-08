7cc8491f601986e14e8c86f8b42c535bc085f6fb
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-08 13:08:25 -0300

    clase 31

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require("http").Server();
+const io = require("socket.io")(server);
+const port = require("./config").SERVER_PORT;
+
+const banner = `
+
+***************
+Basic Nodejs course
+Course project
+Tic tac toe server
+***************
+Status: Online
+Lsitening on port: ${port}
+`;
+
+io.on("connection", socket => {
+  socket.on("register",
+    (user) => {
+      console.info(`User registred: ${user.name}`);
+    }
+    )});
+
+server.listen(port, () => {
+  console.info(banner);
+});
