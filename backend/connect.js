a733a35c7fe89785d3bccdbe2f2c0803afaf3084
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-12 12:00:33 -0300

    Clase 33

--- backend/connect.js
+++ backend/connect.js
@@ -1,15 +1,22 @@
+const matching = require("./matching")();
+
 module.exports = function(io) {
-	io.on("connection", (socket) => {
-		socket.on("register", (user) => {
-			console.info(`User registered: {name: ${user.name}, id: ${user.id}}`)
-		});
-		socket.on("disconnect", ()=>{
-			console.info(`User with ID: ${socket.id} has disconnected.`);
-		});
-		socket.on("message", (id, msg) => {
-			console.log(id, msg);
-			console.info(`Message sent to ${id}`);
-			socket.to(id).emit(msg.type, msg.body);
-		});
-	});
+  io.on("connection", socket => {
+    socket.on("register", user => {
+      console.info(`User registered: {name: ${user.name}, id: ${user.id}}`);
+      matching.userConnect({
+        socket,
+        user
+      });
+    });
+    socket.on("disconnect", () => {
+      matching.userDisconnect(socket.id);
+      console.info(`User with ID: ${socket.id} has disconnected.`);
+    });
+    socket.on("message", (id, msg) => {
+      console.log(id, msg);
+      console.info(`Message sent to ${id}`);
+      socket.to(id).emit(msg.type, msg.body);
+    });
+  });
 };
--- backend/matching.js
+++ backend/matching.js
@@ -1,44 +1,74 @@
 module.exports = () => {
-	let players = {},
-		onWait = [],
-		onMatch = {};
+  let players = {},
+    onWait = [],
+    onMatch = {};
 
-	/**
-	 * 
-	 * Comience aqui
-	 * 
-	 */
+  const loop = setInterval(checkQueue, 5000);
 
-	return {
-		// user: {socket, user}
-		userConnect: ({ socket, user }) => {
-			if (!players[socket.id]) {
-				// Add to player list
-				players[socket.id] = { user, socket };
-				// Add to waiting list
-				onWait.push(socket.id);
-			}
-		},
-		clear: () => clearInterval(loop),
-		userDisconnect: (id) => {
-			// Close ongoing game related to player if any
-			console.log("On disconnect", id);
-			if (players[id].roomID && onMatch[players[id].roomID]) {
-				const roomID = players[id].roomID;
-				// Put all players back on onWait
-				onMatch[roomID].players.map(player => onWait.push(player.id));
-				// Delete match room
-				delete onMatch[players[id].roomID];
-				// If the object gets deleted, reset it
-				if (!onMatch) onMatch = {};
-			}
-			// Delete all instances of disconnecting player from waiting list (if any)
-			onWait = onWait.filter(el => el !== id);
-			// Delete from players list
-			if (players[id]) {
-				delete players[id];
-				if (!players) players = {};
-			}
-		},
-	}
+  function checkQueue() {
+    console.info(
+      `Queues: {Players: ${Object.keys(players).length}, OnWait: ${
+        onWait.length
+      }, OnMatch: ${onMatch.length}}`
+    );
+
+    console.log("Players:");
+    console.log(players);
+    console.log("onWait:");
+    console.log(onWait);
+    console.log("onMatch:");
+    console.log(onMatch);
+
+    //Imprima valores del pool
+    while (onWait.length >= 2) {
+      console.log("Constructing room...");
+      const p1 = players[onWait.pop()].user.name;
+      const p2 = players[onWait.pop()].user.name;
+      console.log(`We created a match for ${p1} and ${p2}`);
+    }
+  }
+
+  function createMatch(p1ID, p2ID) {
+    const roomID = p1ID + P2ID;
+    players[P1ID].roomID = roomID;
+    players[P2ID].roomID = roomID;
+
+    if (!onMatch[roomID]) onMatch[roomID] = {};
+
+    players[p1ID].socket.emit("gameState", {});
+    players[p2ID].socket.emit("gameState", {});
+  }
+
+  return {
+    // user: {socket, user}
+    userConnect: ({ socket, user }) => {
+      if (!players[socket.id]) {
+        // Add to player list
+        players[socket.id] = { user, socket };
+        // Add to waiting list
+        onWait.push(socket.id);
+      }
+    },
+    clear: () => clearInterval(loop),
+    userDisconnect: id => {
+      // Close ongoing game related to player if any
+      console.log("On disconnect", id);
+      if (players[id].roomID && onMatch[players[id].roomID]) {
+        const roomID = players[id].roomID;
+        // Put all players back on onWait
+        onMatch[roomID].players.map(player => onWait.push(player.id));
+        // Delete match room
+        delete onMatch[players[id].roomID];
+        // If the object gets deleted, reset it
+        if (!onMatch) onMatch = {};
+      }
+      // Delete all instances of disconnecting player from waiting list (if any)
+      onWait = onWait.filter(el => el !== id);
+      // Delete from players list
+      if (players[id]) {
+        delete players[id];
+        if (!players) players = {};
+      }
+    }
+  };
 };
